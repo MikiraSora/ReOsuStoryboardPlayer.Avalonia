2f192a0aefa6485288851f6d38c3ffdf4d16cbb1
MikiraSora <mikirasora0409@126.com>
2025-10-15 11:34:29 +0800

    prepare

--- ReOsuStoryboardPlayer.Avalonia/ViewModels/Pages/Home/HomePageViewModel.cs
+++ ReOsuStoryboardPlayer.Avalonia/ViewModels/Pages/Home/HomePageViewModel.cs
@@ -30,6 +30,23 @@ public partial class HomePageViewModel : PageViewModelBase
 
     [ObservableProperty]
     private StoryboardPlayerSetting storyboardPlayerSetting;
+    
+    public string ProgramCommitId => ThisAssembly.GitCommitId;
+    public string ProgramCommitIdShort => ProgramCommitId[..7];
+    public string AssemblyVersion => ThisAssembly.AssemblyVersion;
+    public DateTime ProgramCommitDate => ThisAssembly.GitCommitDate + TimeSpan.FromHours(8);
+    public string ProgramBuildConfiguration => ThisAssembly.AssemblyConfiguration;
+
+    public string ProgramBuildTime
+    {
+        get
+        {
+            var type = typeof(HomePageViewModel).Assembly.GetType("DpMapSubscribeTool.BuildTime");
+            var prop = type?.GetField("Value")?.GetValue(null)
+                ?.ToString();
+            return prop;
+        }
+    }
 
     public HomePageViewModel(
         IDialogManager dialogManager,
