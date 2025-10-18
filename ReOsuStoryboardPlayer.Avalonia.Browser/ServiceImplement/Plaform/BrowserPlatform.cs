3dce29e8b53d077e21912023314ceec0ee12edf3
MikiraSora <mikirasora0409@126.com>
2025-10-18 08:27:08 +0800

    support aot for browser

--- ReOsuStoryboardPlayer.Avalonia.Browser/ServiceImplement/Plaform/BrowserPlatform.cs
+++ ReOsuStoryboardPlayer.Avalonia.Browser/ServiceImplement/Plaform/BrowserPlatform.cs
@@ -1,10 +1,36 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using ReOsuStoryboardPlayer.Avalonia.Utils.Injections;
+﻿using System;
+using System.Threading;
+using Injectio.Attributes;
+using Microsoft.Extensions.Logging;
+using ReOsuStoryboardPlayer.Avalonia.Utils.MethodExtensions;
 
 namespace ReOsuStoryboardPlayer.Avalonia.Services.Plaform;
 
-[Injectio.Attributes.RegisterSingleton<IPlatform>]
-public class BrowserPlatform : IPlatform
+[RegisterSingleton<IPlatform>]
+public class BrowserPlatform(ILogger<BrowserPlatform> logger) : IPlatform
 {
-    public bool SupportMultiThread => false;
+    private bool? supportMultiThread;
+    public bool SupportMultiThread => supportMultiThread ??= CheckIfSupportMultiThread();
+
+    private bool CheckIfSupportMultiThread()
+    {
+        try
+        {
+            var success = false;
+            var ctid = Thread.CurrentThread.ManagedThreadId;
+            var t = new Thread(() =>
+            {
+                var rtid = Thread.CurrentThread.ManagedThreadId;
+                success = ctid != rtid;
+            });
+            t.Start();
+            t.Join(100);
+            return success;
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarningEx($"throw exception: {ex.Message}");
+            return false;
+        }
+    }
 }
--- ReOsuStoryboardPlayer.Avalonia/UI/ValueConverters/RadioButtonEnumToBoolConverter.cs
+++ ReOsuStoryboardPlayer.Avalonia/UI/ValueConverters/RadioButtonEnumToBoolConverter.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Globalization;
 using Avalonia.Data;
-using Microsoft.Extensions.Logging;
 using ReOsuStoryboardPlayer.Avalonia.Utils.Injections;
 using ReOsuStoryboardPlayer.Avalonia.Utils.MethodExtensions;
 
 namespace ReOsuStoryboardPlayer.Avalonia.UI.ValueConverters;
 
 [Injectio.Attributes.RegisterScoped<IInjectableValueConverter>]
-public class RadioButtonEnumToBoolConverter(ILogger<RadioButtonEnumToBoolConverter> logger) : IInjectableValueConverter
+public class RadioButtonEnumToBoolConverter : IInjectableValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        //logger.LogDebugEx($"value: {value}, targetType: {targetType.Name}, parameter: {parameter}, culture: {culture}");
         return value?.Equals(parameter);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        //logger.LogDebugEx($"value: {value}, targetType: {targetType.Name}, parameter: {parameter}, culture: {culture}");
         return value?.Equals(true) == true ? parameter : BindingOperations.DoNothing;
     }
 }
