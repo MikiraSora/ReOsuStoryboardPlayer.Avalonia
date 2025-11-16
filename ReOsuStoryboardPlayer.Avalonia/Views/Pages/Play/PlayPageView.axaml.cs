b96fde60a3b32ef9c123cafef584ca8b840e8f79
RERASER <2641320887@qq.com>
2025-11-17 03:41:46 +0800

    Improve audio playback timing and UI sync
    Refactored AudioClientProvider to use Stopwatch for accurate playback timing and thread-safe state changes. Updated PlayPageViewModel and PlayPageView to synchronize UI updates with audio playback using Avalonia's compositor, ensuring CurrentAudioTime reflects real-time progress. Added System.Numerics.Tensors dependency for optimized audio mixing.

--- ReOsuStoryboardPlayer.Avalonia/Views/Pages/Play/PlayPageView.axaml.cs
+++ ReOsuStoryboardPlayer.Avalonia/Views/Pages/Play/PlayPageView.axaml.cs
@@ -1,9 +1,24 @@
+using Avalonia.Rendering.Composition;
+using ReOsuStoryboardPlayer.Avalonia.ViewModels.Pages.Play;
+
 namespace ReOsuStoryboardPlayer.Avalonia.Views.Pages.Play;
 
 public partial class PlayPageView : PageViewBase
 {
+    private Compositor compositor;
     public PlayPageView()
     {
         InitializeComponent();
+
+        this.Loaded += PlayPageView_Loaded;
+    }
+
+    private void PlayPageView_Loaded(object sender, global::Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        compositor = ElementComposition.GetElementVisual(this).Compositor;
+        if (DataContext is PlayPageViewModel pageViewModel)
+        {
+            compositor.RequestCompositionUpdate(() => { pageViewModel.UpdateCallbackEntryPoint(compositor); });
+        }
     }
 }
